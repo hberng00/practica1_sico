16c9452a220fb6f1546f31b4a4fc175e70b8a095
hberng00 <hberng00@estudiantes.unileon.es>
2024-10-15 18:39:25 +0200

    LF

--- dmz/dmz2/start.sh
+++ dmz/dmz2/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+
 route add default gw 10.5.1.1 
 route del default gw 10.5.1.254 
+
 /usr/sbin/sshd-D
--- external/start.sh
+++ external/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+
 route add default gw 10.5.0.1
 route del default gw 10.5.0.254 
+
 /usr/sbin/sshd-D
