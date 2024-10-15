ba127e3e213e16ab59214108f5d038cd73a0cb38
hberng00 <hberng00@estudiantes.unileon.es>
2024-10-15 18:45:07 +0200

    added space -D

--- external/start.sh
+++ external/start.sh
@@ -3,4 +3,4 @@
 route add default gw 10.5.0.1
 route del default gw 10.5.0.254 
 
-/usr/sbin/sshd-D
+/usr/sbin/sshd -D
--- internal/internal12/start.sh
+++ internal/internal12/start.sh
@@ -3,4 +3,4 @@
 route add default gw 10.5.2.1
 route del default gw 10.5.2.254 
 
-/usr/sbin/sshd-D
+/usr/sbin/sshd -D
