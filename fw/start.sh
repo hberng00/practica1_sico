3704fe3e82c6a621e29b0dc56a65c13c241168a1
hberng00 <hberng00@estudiantes.unileon.es>
2024-10-29 22:04:05 +0100

    Iptables 1

--- fw/start.sh
+++ fw/start.sh
@@ -1,3 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
 
+# Aplicar los cambios de sysctl
+sysctl -p
+
+# Configurar políticas por defecto de iptables
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+
+
+# Permitir tráfico relacionado y establecido
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# Permitir tráfico loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Permitir tráfico ICMP
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+
+# Iniciar el servicio SSH
 /usr/sbin/sshd -D
